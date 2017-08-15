6d5b41d88642a942577469f30fab1066fd90d58f
Lawal <lahamisuahmed@gmail.com>
2017-08-15 15:46:22 +0100

    Change users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', ['male', 'female']);
+            $table-.string('mobile');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
